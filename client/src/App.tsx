41269fedc386e2e63c7fdfb2f6cef8f45c2604f9
lychee-55 <zylee503@naver.com>
2025-03-30 18:21:36 +0900

    project setting

---
+++ client/src/App.tsx
@@ -0,0 +1,8 @@
+// import './App.css';
+import '../src/style/main.css';
+
+function App() {
+  return <div className="App">1</div>;
+}
+
+export default App;
---
+++ client/src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import '../src/style/main.css';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement,
+);
+root.render(<App />);
